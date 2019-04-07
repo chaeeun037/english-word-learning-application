82cd5cd9614a6da61e4fea74c12ed0ee81b189e2
chaeeun037 <faily037@gmail.com>
2019-04-07 16:53:57 +0900

    초기 화면 개발중..

--- Application/app/src/main/java/com/application/MainActivity.java
+++ Application/app/src/main/java/com/application/MainActivity.java
@@ -1,13 +1,29 @@
 package com.application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_learning, btn_game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_learning = (Button)findViewById(R.id.btn_learning);
+        btn_game= (Button)findViewById(R.id.btn_game);
+
+        btn_learning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

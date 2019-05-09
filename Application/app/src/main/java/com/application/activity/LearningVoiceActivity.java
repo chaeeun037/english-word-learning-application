49eadad6b16d4726fa2b28451e982aa3191f617b
wnthgus0222 <wnthgus0222@naver.com>
2019-05-09 17:34:08 +0900

    화살표 이모티콘 추가

--- Application/app/src/main/java/com/application/activity/LearningVoiceActivity.java
+++ Application/app/src/main/java/com/application/activity/LearningVoiceActivity.java
@@ -23,7 +23,7 @@ public class LearningVoiceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_learning_voice);
 
         speakButton = (Button) findViewById(R.id.speakButton);
 

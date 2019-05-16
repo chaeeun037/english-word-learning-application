505c4e73814462732ff666c69570b148a897155c
MangNyang <sharonso@naver.com>
2019-05-16 15:26:51 +0900

    finish make database create and drop, plz interlock

--- Application/app/src/main/java/com/application/database/EWLADbHelper.java
+++ Application/app/src/main/java/com/application/database/EWLADbHelper.java
@@ -10,6 +10,23 @@ public class EWLADbHelper extends SQLiteOpenHelper {
 
     private static final int DB_VERSION = 1;
     private static final String DB_NAME = "EWLA.db";
+
+    private static final String SQL_CREATE_THEME_ENTRIES =
+            String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s NUMERIC, %s INTEGER)",
+                    ThemeContract.Theme.TABLE_NAME,
+                    ThemeContract.Theme.THEME_NAME_ID,
+                    ThemeContract.Theme.THEME_NAME_TITLE,
+                    ThemeContract.Theme.THEME_NAME_ISLOCKED,
+                    ThemeContract.Theme.THEME_NAME_UNLOCKPOINT);
+
+    private static final String SQL_CREATE_UNIT_ENTRIES =
+            String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s INTEGER, %s TEXT, %s NUMERIC)",
+                    UnitContract.Unit.TABLE_NAME,
+                    UnitContract.Unit.UNIT_NAME_THEME_ID,
+                    UnitContract.Unit.UNIT_NAME_ID,
+                    UnitContract.Unit.UNIT_NAME_TITLE,
+                    UnitContract.Unit.UNIT_NAME_HASCROWN);
+
     private static final String SQL_CREATE_WORD_ENTRIES =
             String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s INTEGER, %s TEXT, %s TEXT, %s TEXT, %s TEXT)",
                     WordContract.WordEntry.TABLE_NAME,
@@ -20,8 +37,15 @@ public class EWLADbHelper extends SQLiteOpenHelper {
                     WordContract.WordEntry.COLUMN_NAME_ENGLISH,
                     WordContract.WordEntry.COLUMN_NAME_SHADOW_SRC);
 
+
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + WordContract.WordEntry.TABLE_NAME;
+
+    private static final String SQL_DELETE_THEME =
+            "DROP TABLE IF EXISTS " + ThemeContract.Theme.TABLE_NAME;
+
+    private static final String SQL_DELETE_UNIT =
+            "DROP TABLE IF EXISTS " + UnitContract.Unit.TABLE_NAME;
 
     public static EWLADbHelper getsInstance(Context context) {
         if (sInstance == null) {
@@ -36,13 +60,19 @@ public class EWLADbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
         db.execSQL(SQL_CREATE_WORD_ENTRIES);
+        db.execSQL(SQL_CREATE_THEME_ENTRIES);
+        db.execSQL(SQL_CREATE_UNIT_ENTRIES);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (newVersion == DB_VERSION) {
             db.execSQL(SQL_DELETE_ENTRIES);
+            db.execSQL(SQL_DELETE_THEME);
+            db.execSQL(SQL_DELETE_UNIT);
             onCreate(db);
         }
     }

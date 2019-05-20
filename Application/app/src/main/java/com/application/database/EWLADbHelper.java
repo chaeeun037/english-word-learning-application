1d3e16bfd1e1d82d244c584e3fc5703cf3c36088
MangNyang <sharonso@naver.com>
2019-05-20 17:56:13 +0900

    database 연동 완료

--- Application/app/src/main/java/com/application/database/EWLADbHelper.java
+++ Application/app/src/main/java/com/application/database/EWLADbHelper.java
@@ -1,79 +1,119 @@
 package com.application.database;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 // 해당 클래스는 helper로 데이터베이스를 컨넥션 연동 시키기 위해 제작된 클래스입니다.
 public class EWLADbHelper extends SQLiteOpenHelper {
-    private static EWLADbHelper sInstance;
 
     private static final int DB_VERSION = 1;
-    private static final String DB_NAME = "EWLA.db";
+    private Context mycontext;
+    private static String DB_NAME = "EWLA.db";
+    public SQLiteDatabase myDatabase;
+    private String DB_PATH = "/data/data/"
+            + mycontext.getApplicationContext().getPackageName()
+            + "/databases/";
 
-    private static final String SQL_CREATE_THEME_ENTRIES =
-            String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s NUMERIC, %s INTEGER)",
-                    ThemeContract.Theme.TABLE_NAME,
-                    ThemeContract.Theme.THEME_NAME_ID,
-                    ThemeContract.Theme.THEME_NAME_TITLE,
-                    ThemeContract.Theme.THEME_NAME_ISLOCKED,
-                    ThemeContract.Theme.THEME_NAME_UNLOCKPOINT);
+    private static EWLADbHelper sInstance;
 
-    private static final String SQL_CREATE_UNIT_ENTRIES =
-            String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s INTEGER, %s TEXT, %s NUMERIC)",
-                    UnitContract.Unit.TABLE_NAME,
-                    UnitContract.Unit.UNIT_NAME_THEME_ID,
-                    UnitContract.Unit.UNIT_NAME_ID,
-                    UnitContract.Unit.UNIT_NAME_TITLE,
-                    UnitContract.Unit.UNIT_NAME_HASCROWN);
+    public EWLADbHelper(Context context) throws IOException {
+        super(context, DB_NAME, null, 1);
+        this.mycontext = context;
+        boolean dbexist = checkdatabase();
+        if (dbexist) {
+            //System.out.println("Database exists");
+            opendatabase();
+        } else {
+            System.out.println("Database doesn't exist");
+            createdatabase();
+        }
+    }
 
-    private static final String SQL_CREATE_WORD_ENTRIES =
-            String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s INTEGER, %s TEXT, %s TEXT, %s TEXT, %s TEXT)",
-                    WordContract.WordEntry.TABLE_NAME,
-                    WordContract.WordEntry._ID,
-                    WordContract.WordEntry.COLUMN_NAME_UNIT_ID,
-                    WordContract.WordEntry.COLUMN_NAME_IMAGE_SRC,
-                    WordContract.WordEntry.COLUMN_NAME_KOREAN,
-                    WordContract.WordEntry.COLUMN_NAME_ENGLISH,
-                    WordContract.WordEntry.COLUMN_NAME_SHADOW_SRC);
+    private boolean checkdatabase() {
+        //SQLiteDatabase checkdb = null;
+        boolean checkdb = false;
+        try {
+            String myPath = DB_PATH + DB_NAME;
+            File dbfile = new File(myPath);
+            //checkdb = SQLiteDatabase.openDatabase(myPath,null,SQLiteDatabase.OPEN_READWRITE);
+            checkdb = dbfile.exists();
+        } catch (SQLiteException e) {
+            System.out.println("Database doesn't exist");
+        }
+        return checkdb;
+    }
 
+    private void copydatabase() throws IOException {
 
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + WordContract.WordEntry.TABLE_NAME;
+        //Open your local db as the input stream
+        InputStream myinput = mycontext.getAssets().open(DB_NAME);
 
-    private static final String SQL_DELETE_THEME =
-            "DROP TABLE IF EXISTS " + ThemeContract.Theme.TABLE_NAME;
+        // Path to the just created empty db
+        String outfilename = DB_PATH + DB_NAME;
 
-    private static final String SQL_DELETE_UNIT =
-            "DROP TABLE IF EXISTS " + UnitContract.Unit.TABLE_NAME;
+        //Open the empty db as the output stream
+        OutputStream myoutput = new FileOutputStream("/data/data/com.application/databases/EWLA.db");
 
-    public static EWLADbHelper getsInstance(Context context) {
+        // transfer byte to inputfile to outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myinput.read(buffer)) > 0) {
+            myoutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myoutput.flush();
+        myoutput.close();
+        myinput.close();
+
+    }
+
+    public void opendatabase() throws SQLException {
+        //Open the database
+        String mypath = DB_PATH + DB_NAME;
+        myDatabase = SQLiteDatabase.openDatabase(mypath, null, SQLiteDatabase.OPEN_READWRITE);
+
+    }
+
+    public void createdatabase() throws IOException {
+        boolean dbexist = checkdatabase();
+        if (dbexist) {
+            //System.out.println(" Database exists.");
+        } else {
+            this.getReadableDatabase();
+            try {
+                copydatabase();
+            } catch (IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+    }
+
+    public static EWLADbHelper getsInstance(Context context) throws IOException {
         if (sInstance == null) {
             sInstance = new EWLADbHelper(context);
         }
         return sInstance;
     }
 
-    private EWLADbHelper(Context context) {
-        super(context, DB_NAME, null, DB_VERSION);
-    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL(SQL_CREATE_WORD_ENTRIES);
-        db.execSQL(SQL_CREATE_THEME_ENTRIES);
-        db.execSQL(SQL_CREATE_UNIT_ENTRIES);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (newVersion == DB_VERSION) {
-            db.execSQL(SQL_DELETE_ENTRIES);
-            db.execSQL(SQL_DELETE_THEME);
-            db.execSQL(SQL_DELETE_UNIT);
-            onCreate(db);
-        }
+
     }
 }

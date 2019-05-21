9a8c59f90ff4d68c6d7aa902924dbee0f812de3d
MangNyang <sharonso@naver.com>
2019-05-21 18:45:39 +0900

    데이터베이스에 임시 정보 추가

--- Application/app/src/main/java/com/application/database/EWLADbHelper.java
+++ Application/app/src/main/java/com/application/database/EWLADbHelper.java
@@ -1,12 +1,9 @@
 package com.application.database;
 
 import android.content.Context;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,105 +12,59 @@ import java.io.OutputStream;
 // 해당 클래스는 helper로 데이터베이스를 컨넥션 연동 시키기 위해 제작된 클래스입니다.
 public class EWLADbHelper extends SQLiteOpenHelper {
 
-    private static final int DB_VERSION = 1;
-    private Context mycontext;
-    private static String DB_NAME = "EWLA.db";
-    public SQLiteDatabase myDatabase;
-    private String DB_PATH = "/data/data/"
-            + mycontext.getApplicationContext().getPackageName()
-            + "/databases/";
-
     private static EWLADbHelper sInstance;
+    private Context mycontext;
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "EWLA.db";
 
-    public EWLADbHelper(Context context) throws IOException {
-        super(context, DB_NAME, null, 1);
-        this.mycontext = context;
-        boolean dbexist = checkdatabase();
-        if (dbexist) {
-            //System.out.println("Database exists");
-            opendatabase();
-        } else {
-            System.out.println("Database doesn't exist");
-            createdatabase();
-        }
-    }
 
-    private boolean checkdatabase() {
-        //SQLiteDatabase checkdb = null;
-        boolean checkdb = false;
-        try {
-            String myPath = DB_PATH + DB_NAME;
-            File dbfile = new File(myPath);
-            //checkdb = SQLiteDatabase.openDatabase(myPath,null,SQLiteDatabase.OPEN_READWRITE);
-            checkdb = dbfile.exists();
-        } catch (SQLiteException e) {
-            System.out.println("Database doesn't exist");
-        }
-        return checkdb;
-    }
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + WordContract.WordEntry.TABLE_NAME;
 
-    private void copydatabase() throws IOException {
+    private static final String SQL_DELETE_THEME =
+            "DROP TABLE IF EXISTS " + ThemeContract.Theme.TABLE_NAME;
 
-        //Open your local db as the input stream
-        InputStream myinput = mycontext.getAssets().open(DB_NAME);
+    private static final String SQL_DELETE_UNIT =
+            "DROP TABLE IF EXISTS " + UnitContract.Unit.TABLE_NAME;
 
-        // Path to the just created empty db
-        String outfilename = DB_PATH + DB_NAME;
-
-        //Open the empty db as the output stream
-        OutputStream myoutput = new FileOutputStream("/data/data/com.application/databases/EWLA.db");
-
-        // transfer byte to inputfile to outputfile
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = myinput.read(buffer)) > 0) {
-            myoutput.write(buffer, 0, length);
-        }
-
-        //Close the streams
-        myoutput.flush();
-        myoutput.close();
-        myinput.close();
-
-    }
-
-    public void opendatabase() throws SQLException {
-        //Open the database
-        String mypath = DB_PATH + DB_NAME;
-        myDatabase = SQLiteDatabase.openDatabase(mypath, null, SQLiteDatabase.OPEN_READWRITE);
-
-    }
-
-    public void createdatabase() throws IOException {
-        boolean dbexist = checkdatabase();
-        if (dbexist) {
-            //System.out.println(" Database exists.");
-        } else {
-            this.getReadableDatabase();
-            try {
-                copydatabase();
-            } catch (IOException e) {
-                throw new Error("Error copying database");
-            }
-        }
-    }
-
-    public static EWLADbHelper getsInstance(Context context) throws IOException {
+    public static EWLADbHelper getsInstance(Context context) {
         if (sInstance == null) {
             sInstance = new EWLADbHelper(context);
         }
         return sInstance;
     }
 
+    private EWLADbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    private void copydatabase() throws IOException {
+        InputStream myInput = mycontext.getAssets().open(DB_NAME);
+        String outFileName = DB_NAME + DB_NAME;
+        OutputStream myOutput = new FileOutputStream("/data/data/com.application/databases/EWLA.db");
+        byte[] buffer = new  byte[1024];
+        int length;
+        while((length = myInput.read(buffer))>0)
+            myOutput.write(buffer, 0, length);
+
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        if (newVersion == DB_VERSION) {
+            db.execSQL(SQL_DELETE_ENTRIES);
+            db.execSQL(SQL_DELETE_THEME);
+            db.execSQL(SQL_DELETE_UNIT);
+            onCreate(db);
+        }
     }
 }

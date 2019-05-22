04477e8ed4867c11d5d25e9ee84c1107ad5817f7
jisoo920 <kangjs920@gmail.com>
2019-05-22 14:31:58 +0900

    캔버스 활성화

--- Application/app/src/main/java/com/application/fragment/DrawCanvasView.java
+++ Application/app/src/main/java/com/application/fragment/DrawCanvasView.java
@@ -1,4 +1,121 @@
 package com.application.fragment;
 
-public class drawCanvasView {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawCanvasView extends View {
+
+    private Path drawPath;
+    private Paint drawPaint, canvasPaint;
+    private int paintColor = Color.BLACK; //펜색
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+
+    public DrawCanvasView(Context context) {
+        super(context);
+        this.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        //setupDrawing(); // draw의 시작
+    }
+
+    public DrawCanvasView(Context context, AttributeSet attributeSet){
+        super(context, attributeSet);
+        this.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    /* drawing을 위한 set up */
+    private void setupDrawing() {
+        drawPath = new Path(); // 이미 그어진 선을 저장하기 위한 객체
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true); //경계면을 부드럽게 처리
+        drawPaint.setStrokeWidth(25); // 펜 굵기
+        drawPaint.setStyle(Paint.Style.STROKE); // 선을 긋게 하는 부분
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    /* 화면을 그려주는 메소드 */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //super.onDraw(canvas);
+        Paint paint = new Paint();
+        setupDrawing();
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        //  canvas.drawPath(drawPath, drawPaint); // 저장된 path를 그리는 ~
+    }
+
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    /* Touch 입력 받기 */
+    int oldX = -1, oldY = -1;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int touchX = (int)event.getX();
+        int touchY = (int)event.getY();
+
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            oldX = touchX;
+            oldY = touchY;
+        }
+        else if(event.getAction() == MotionEvent.ACTION_MOVE){
+            if(oldX != -1){
+                drawCanvas.drawLine(oldX, oldY, touchX, touchY, drawPaint);
+                invalidate();
+                oldX = touchX;
+                oldY = touchY;
+            }
+        }
+        else if(event.getAction() == MotionEvent.ACTION_UP){
+            if(oldX != -1){
+                drawCanvas.drawLine(oldX, oldY, touchX, touchY, drawPaint);
+                invalidate();
+            }
+            oldX = -1;
+            oldY = -1;
+        }
+        return true;
+
+        /*
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN://누를 때
+                drawPath.moveTo(touchX, touchY); // 위치 이동
+                break;
+
+            case MotionEvent.ACTION_MOVE://드래그
+                drawPath.lineTo(touchX, touchY);// 위치에 선을 남김
+                break;
+
+            case MotionEvent.ACTION_UP://드롭
+                drawPath.lineTo(touchX, touchY);
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+
+                break;
+
+            default:
+                return false;
+        }
+
+        invalidate(); //화면을 다시 생성
+        return true;
+        */
+
+    }
+
+    public void eraseAll() {
+        drawCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR); // 캔버스 초기화 함수(지우개)
+    }
 }
--- Application/app/src/main/java/com/application/fragment/LearningHandwriteFragment.java
+++ Application/app/src/main/java/com/application/fragment/LearningHandwriteFragment.java
@@ -1,14 +1,31 @@
 package com.application.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.application.R;
 
+/*
+ * 지수 작성
+ * < 손글씨 화면 거기>
+ * 1. 캔버스 나오게
+ * 2. 그림자 글씨 나오게
+ * 3. 지우개 버튼 먹게*
+ *
+ * 20190521 - 화면은 뜨는데 뷰가 생기지는 X
+ */
+
 public class LearningHandwriteFragment extends Fragment {
+    DrawCanvasView shadowCanvasV;
+
+    public LearningHandwriteFragment() {
+    }
 
     public static LearningSummaryFragment newInstance() {
         return new LearningSummaryFragment();
@@ -17,7 +34,62 @@ public class LearningHandwriteFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_learning_handwrite, container, false);
+        //shadowCanvasV= new DrawCanvasView(getContext()); //캔버스
+        init(view);
+        shadowCanvasV = (DrawCanvasView) view.findViewById(R.id.shadowCanvas);
 
-        return inflater.inflate(R.layout.fragment_learning_handwrite, container, false);
+        //Button 정의 ****추후 수정
+        Button pen = (Button)view.findViewById(R.id.penButton);
+        Button eraser = (Button) view.findViewById(R.id.eraserButton);
+
+
+
+        //LayoutInflater li = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+       /* LinearLayout shadowCanvas = (LinearLayout) view.findViewById(R.id.shadowCanvas); //캔버스가 추가될 linearLayout
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+
+
+        shadowCanvas.addView(shadowCanvasV);
+*/
+
+/*
+        pen.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                LayoutInflater inflater1 = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                LinearLayout a = (LinearLayout) inflater1.inflate(R.layout.fragment_learning_handwrite, null);
+                shadowCanvas.addView(a);
+                }
+
+        });
+
+        minianvas.addView(shadowCanvasV);
+        */
+
+        return view;
+    }
+
+    public void init(View view) {
+        shadowCanvasV = (DrawCanvasView) view.findViewById(R.id.shadowCanvas);
+    }
+
+    /* 버튼을 클릭하면 뷰가 생성되도록 하고 싶었으나... */
+    public void penButtonClicked(){
+        /*
+        LinearLayout miniCanvas = (LinearLayout)getView().findViewById(R.id.shadowCanvas);
+        LayoutInflater inflater1 = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.fragment_learning_handwrite, container, false);
+        miniCanvas.addView(view);
+        */
+    }
+
+    public void eraserButtonClicked(){
+        shadowCanvasV.eraseAll();
     }
 }

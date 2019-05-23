223993f1076105cdf5857044dde339b0c679af80
chaeeun037 <faily037@gmail.com>
2019-05-23 16:17:22 +0900

    소연이가 merge하기 직전 코드

--- Application/app/src/main/java/com/application/database/EWLADbHelper.java
+++ Application/app/src/main/java/com/application/database/EWLADbHelper.java
@@ -10,26 +10,6 @@ public class EWLADbHelper extends SQLiteOpenHelper {
 
     private static final int DB_VERSION = 1;
     private static final String DB_NAME = "EWLA.db";
-<<<<<<< HEAD
-
-    private static final String SQL_CREATE_THEME_ENTRIES =
-            String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s NUMERIC, %s INTEGER)",
-                    ThemeContract.Theme.TABLE_NAME,
-                    ThemeContract.Theme.THEME_NAME_ID,
-                    ThemeContract.Theme.THEME_NAME_TITLE,
-                    ThemeContract.Theme.THEME_NAME_ISLOCKED,
-                    ThemeContract.Theme.THEME_NAME_UNLOCKPOINT);
-
-    private static final String SQL_CREATE_UNIT_ENTRIES =
-            String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s INTEGER, %s TEXT, %s NUMERIC)",
-                    UnitContract.Unit.TABLE_NAME,
-                    UnitContract.Unit.UNIT_NAME_THEME_ID,
-                    UnitContract.Unit.UNIT_NAME_ID,
-                    UnitContract.Unit.UNIT_NAME_TITLE,
-                    UnitContract.Unit.UNIT_NAME_HASCROWN);
-
-=======
->>>>>>> 8d7deac... 앱 이름 MARKET GO로 변경 / DB 연결 중...
     private static final String SQL_CREATE_WORD_ENTRIES =
             String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s INTEGER, %s TEXT, %s TEXT, %s TEXT, %s TEXT)",
                     WordContract.WordEntry.TABLE_NAME,
@@ -40,22 +20,10 @@ public class EWLADbHelper extends SQLiteOpenHelper {
                     WordContract.WordEntry.COLUMN_NAME_ENGLISH,
                     WordContract.WordEntry.COLUMN_NAME_SHADOW_SRC);
 
-<<<<<<< HEAD
 
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + WordContract.WordEntry.TABLE_NAME;
 
-    private static final String SQL_DELETE_THEME =
-            "DROP TABLE IF EXISTS " + ThemeContract.Theme.TABLE_NAME;
-
-    private static final String SQL_DELETE_UNIT =
-            "DROP TABLE IF EXISTS " + UnitContract.Unit.TABLE_NAME;
-
-=======
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + WordContract.WordEntry.TABLE_NAME;
-
->>>>>>> 8d7deac... 앱 이름 MARKET GO로 변경 / DB 연결 중...
     public static EWLADbHelper getsInstance(Context context) {
         if (sInstance == null) {
             sInstance = new EWLADbHelper(context);
@@ -69,26 +37,15 @@ public class EWLADbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-<<<<<<< HEAD
 
         db.execSQL(SQL_CREATE_WORD_ENTRIES);
-        db.execSQL(SQL_CREATE_THEME_ENTRIES);
-        db.execSQL(SQL_CREATE_UNIT_ENTRIES);
-
-=======
-        db.execSQL(SQL_CREATE_WORD_ENTRIES);
->>>>>>> 8d7deac... 앱 이름 MARKET GO로 변경 / DB 연결 중...
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (newVersion == DB_VERSION) {
             db.execSQL(SQL_DELETE_ENTRIES);
-<<<<<<< HEAD
-            db.execSQL(SQL_DELETE_THEME);
-            db.execSQL(SQL_DELETE_UNIT);
-=======
->>>>>>> 8d7deac... 앱 이름 MARKET GO로 변경 / DB 연결 중...
+
             onCreate(db);
         }
     }

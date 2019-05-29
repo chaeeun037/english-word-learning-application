63ef2eb530634481ef00a7385f3843504a74e978
chaeeun037 <faily037@gmail.com>
2019-05-30 05:51:53 +0900

    game 부분 애니메이션 효과 추가

--- Application/app/src/main/java/com/application/fragment/SpeakMainFragment.java
+++ Application/app/src/main/java/com/application/fragment/SpeakMainFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.application.R;
 
